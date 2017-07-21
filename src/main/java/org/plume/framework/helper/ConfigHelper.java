a4076080ed4d018caa909e005066007f52550122
nowayingswang <nowayings@gmail.com>
2017-07-21 14:21:21 +0800

    【添加】完成读取配置信息类 configHelper

--- src/main/java/org/plume/framework/helper/ConfigHelper.java
+++ src/main/java/org/plume/framework/helper/ConfigHelper.java
@@ -26,4 +26,39 @@ public class ConfigHelper {
         return PropsUtil.getString(CONFIG_PROPS,ConfigConstant.JDBC_URL);
     }
 
+    /**
+     * 获取jdbc用户名
+     */
+    public static String getJdbcUsername(){
+        return PropsUtil.getString(CONFIG_PROPS,ConfigConstant.JDBC_USERNAME);
+    }
+
+    /**
+     * 获取jdbc密码
+     */
+    public static String getJdbcPassword(){
+        return PropsUtil.getString(CONFIG_PROPS,ConfigConstant.JDBC_PASSWORD);
+    }
+
+    /**
+     * 获取应用基础包名
+     */
+    public static String getAppBasePackage(){
+        return PropsUtil.getString(CONFIG_PROPS,ConfigConstant.APP_BASE_PACKAGE);
+    }
+
+    /**
+     * 获取应用jsp路径信息
+     */
+    public static String getAppJspPath(){
+        return PropsUtil.getString(CONFIG_PROPS,ConfigConstant.APP_JSP_PATH,"/WEB-INF/view");
+    }
+
+    /**
+     * 获取应用静态资源路径信息
+     */
+    public static String getAppAssetPath(){
+        return PropsUtil.getString(CONFIG_PROPS,ConfigConstant.APP_ASSET_PATH,"/asset");
+    }
+
 }

c05a3621cf31d65e805255610b1e7c1a7e07115b
nowayingswang <nowayings@gmail.com>
2017-07-24 19:57:15 +0800

    【添加】添加ReflectUtil工具类

---
+++ src/main/java/org/plume/framework/utils/ReflectionUtil.java
@@ -0,0 +1,23 @@
+package org.plume.framework.utils;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 反射工具类
+ * Created by wangpf on 2017/7/24.
+ */
+public class ReflectionUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     * @param cls 泛型类名
+     * @return 实例对象
+     */
+    public static Object newInstance(Class<?> cls){
+        //TODO ...
+        return null;
+    }
+}

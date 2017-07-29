b16e68e1ad4667ffd51b72fc66149ae7c4479ee2
nowayingswang <nowayings@gmail.com>
2017-07-29 16:02:31 +0800

    【添加】反射工具类完成

--- src/main/java/org/plume/framework/utils/ReflectionUtil.java
+++ src/main/java/org/plume/framework/utils/ReflectionUtil.java
@@ -1,8 +1,12 @@
 package org.plume.framework.utils;
 
-
+import com.sun.tracing.dtrace.ArgsAttributes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * 反射工具类
@@ -17,7 +21,47 @@ public class ReflectionUtil {
      * @return 实例对象
      */
     public static Object newInstance(Class<?> cls){
-        //TODO ...
-        return null;
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure",e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     * @param obj 调用对象
+     * @param method 调用方法
+     * @return 方法返回结果
+     */
+    public static Object invokeMethod(Object obj, Method method,Object ... args){
+        Object result;
+        method.setAccessible(true);
+        try {
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method faulure",e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量的值
+     * @param obj 需要设置的对象
+     * @param field 成员变量
+     * @param value 成员变量值
+     */
+    public static void setField(Object obj, Field field,Object value){
+        field.setAccessible(true);//打开访问权限控制
+        try {
+            field.set(obj,value);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("set field failure",e);
+            throw new RuntimeException(e);
+        }
     }
 }
